b9a998dd17ef2816e0ec734101869638344db0b3
Finlay Lumsden <Finlay.Lumsden@softwire.com>
2025-07-07 17:30:44 +0100

    Refactored code

--- src/bots/drawBehaviourPrediction.ts
+++ src/bots/drawBehaviourPrediction.ts
@@ -1,86 +1,102 @@
 import { Gamestate, BotSelection } from '../models/gamestate';
 
+enum DrawBehaviour {
+    random,
+    dynamite,
+    waterbomb
+}
+
+const ROCK: BotSelection = 'R', PAPER: BotSelection = 'P', SCISSOR: BotSelection = 'S', DYNAMITE: BotSelection = 'D', WATERBOMB: BotSelection = 'W';
+
 class Bot {
     // Store encoded round outcomes that correlate to a win
-    private roundWinners = new Set<string>(["RS", "SP",
+    private winningMovePairs = new Set<string>(["RS", "SP",
         "PR", "WD", "RW", "PW", "SW", "DR", "DP", "DS"]);
-    private currentNonDrawingRounds = 0;
-    private currentLosingRounds = 0;
-    private pointValue = 1;
-    private previousPointValue = 1;
-    private dynamiteUses: number = 0;
-    private opponentDynamiteUses: number = 0;
-    private opponentDrawBehaviour: number = 0; // 0 random, 1 D, 2 W
+    private undrawnRoundCount = 0;
+    private lostRoundCount = 0;
+    private potentialRoundScore = 1;
+    private previousRoundScore = 1;
+    private usedPlayerDynamite: number = 0;
+    private usedOpponentDynamite: number = 0;
+    private expectedOpponentBehaviour: DrawBehaviour = DrawBehaviour.random; // 0 random, 1 D, 2 W
 
     makeMove(gamestate: Gamestate): BotSelection {
         if (gamestate.rounds.length === 0)
-            return <BotSelection>'RPS'[Math.round(Math.random()*3-0.5)];
+            return this.selectRandomMove();
+        let previousRoundMoves = gamestate.rounds[gamestate.rounds.length-1];
         // Track the current number of non-drawn rounds and lost rounds
-        let roundScoreDelta = this.determineWin(gamestate.rounds[gamestate.rounds.length-1]);
-        if (roundScoreDelta !== 0) {
-            this.currentNonDrawingRounds++;
-            if (roundScoreDelta < 0) {
-                this.currentLosingRounds++;
+        let previousRoundScoreDelta = this.calculateRoundScoreDelta(previousRoundMoves);
+        if (previousRoundScoreDelta !== 0) {
+            this.undrawnRoundCount++;
+            if (previousRoundScoreDelta < 0) {
+                this.lostRoundCount++;
             }
         }
         // After a draw has been resolved, predict opponent draw behaviour
-        if (this.previousPointValue > 1 && this.pointValue === 1) {
-            if (gamestate.rounds[gamestate.rounds.length-1].p2 === "D" && this.opponentDynamiteUses<100) {
-                this.opponentDrawBehaviour = 1;
-            } else if (gamestate.rounds[gamestate.rounds.length-1].p2 === "W" && this.dynamiteUses<100) {
-                this.opponentDrawBehaviour = 2;
-            } else {
-                this.opponentDrawBehaviour = 0;
+        if (this.potentialRoundScore === 1) {
+            if (this.previousRoundScore > 1) {
+                this.updateExpectedOpponentBehaviour(previousRoundMoves);
             }
-        }
-        // Use internal logic to handle a draw
-        if (this.pointValue > 1) {
-            if (this.pointValue > 3 && this.opponentDrawBehaviour === 0 && this.opponentDynamiteUses<100) {
-                return 'W';
+        } else {
+            // Repeated ties when expecting random play implies deliberate opponent dynamite
+            if (this.potentialRoundScore > 3 && this.expectedOpponentBehaviour === DrawBehaviour.random && this.usedOpponentDynamite<100) {
+                return WATERBOMB;
             }
-            if (this.opponentDrawBehaviour === 2) {
-                return <BotSelection>'RPS'[Math.round(Math.random()*3-0.5)];
-            } else if (this.opponentDrawBehaviour === 0) {
-                if (this.dynamiteUses < 100) {
-                    this.dynamiteUses++;
-                    return 'D';
+            if (this.expectedOpponentBehaviour === DrawBehaviour.waterbomb) {
+                return this.selectRandomMove();
+            } else if (this.expectedOpponentBehaviour === DrawBehaviour.random) {
+                if (this.usedPlayerDynamite < 100) {
+                    this.usedPlayerDynamite++;
+                    return DYNAMITE;
                 }
-            } else if (this.opponentDynamiteUses<100){
-                return 'W';
+            } else if (this.usedOpponentDynamite<100){
+                return WATERBOMB;
             }
         }
         // Try and use dynamite more often if not using it enough
-        if (this.dynamiteUses < 100) {
-            let ownLossRate = this.currentLosingRounds / this.currentNonDrawingRounds;
+        if (this.usedPlayerDynamite < 100) {
+            let ownLossRate = this.lostRoundCount / this.undrawnRoundCount;
             let higherWinRate = Math.max(1 - ownLossRate, ownLossRate);
-            let projectedRounds = 1000 / higherWinRate;
-            let remainingRounds = projectedRounds - this.currentNonDrawingRounds;
-            let dynamiteProbability = (100 - this.dynamiteUses) / remainingRounds;
+            let projectedTotalRounds = 1000 / higherWinRate;
+            let projectedRemainingRounds = projectedTotalRounds - this.undrawnRoundCount;
+            let dynamiteProbability = (100 - this.usedPlayerDynamite) / projectedRemainingRounds;
             if (Math.random() < dynamiteProbability) {
-                this.dynamiteUses++;
-                return 'D';
+                this.usedPlayerDynamite++;
+                return DYNAMITE;
             }
         }
-        // Fallback to a random choice
-        return <BotSelection>'RPS'[Math.round(Math.random()*3-0.5)];
+        return this.selectRandomMove();
+    }
+
+    private selectRandomMove() {
+        return [ROCK, PAPER, SCISSOR][Math.round(Math.random()*3-0.5)];
+    }
+
+    private updateExpectedOpponentBehaviour(previousRoundMoves) {
+        if (previousRoundMoves.p2 === DYNAMITE && this.usedOpponentDynamite < 100) {
+            this.expectedOpponentBehaviour = DrawBehaviour.dynamite;
+        } else if (previousRoundMoves.p2 === WATERBOMB && this.usedPlayerDynamite < 100) {
+            this.expectedOpponentBehaviour = DrawBehaviour.waterbomb;
+        } else {
+            this.expectedOpponentBehaviour = DrawBehaviour.random;
+        }
     }
 
     // Return the scoring delta (1 is positive for the bot)
-    private determineWin(previousRound) {
-        this.previousPointValue = this.pointValue;
-        if (previousRound.p2 === "D") {
-            this.opponentDynamiteUses++;
-        }
+    private calculateRoundScoreDelta(previousRound) {
+        this.previousRoundScore = this.potentialRoundScore;
+        if (previousRound.p2 === DYNAMITE)
+            this.usedOpponentDynamite++;
         if (previousRound.p1 === previousRound.p2) {
-            this.pointValue++;
+            this.potentialRoundScore++;
             return 0;
         }
-        let previousValue = this.pointValue;
-        this.pointValue = 1;
-        let previousRoundString = previousRound.p1 + previousRound.p2;
-        if (this.roundWinners.has(previousRoundString))
-            return previousValue;
-        return -previousValue;
+        let previousRoundPotentialScore = this.potentialRoundScore;
+        this.potentialRoundScore = 1;
+        let previousRoundOutcomeString = previousRound.p1 + previousRound.p2;
+        if (this.winningMovePairs.has(previousRoundOutcomeString))
+            return previousRoundPotentialScore;
+        return -previousRoundPotentialScore;
     }
 }
 
